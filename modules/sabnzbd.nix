990bd286e95d750ca3c2c99899e76509600f8250
ArtLiathain <art.oliathain@gmail.com>
2026-05-25 15:49:58 +0100

    fix: Sabnzbd

--- modules/sabnzbd.nix
+++ modules/sabnzbd.nix
@@ -4,10 +4,6 @@
   services.sabnzbd = {
     enable = true;
     openFirewall = true;
-    settings.misc = {
-      download_dir = "/data/usenet/incomplete";
-      complete_dir = "/data/usenet/complete";
-    };
   };
 
   systemd.services.sabnzbd.serviceConfig = {
