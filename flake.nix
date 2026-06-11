d23efe804904b823794d4978576fa3e08b914e91
ArtLiathain <art.oliathain@gmail.com>
2026-06-11 12:54:56 +0100

    fix

--- flake.nix
+++ flake.nix
@@ -1,14 +1,12 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-        dotfiles.url = "path:./dotfiles";
+    dotfiles.url = "path:./dotfiles";
     dotfiles.flake = false;
-self.submodules = true;
   };
 
 
   outputs = { nixpkgs, dotfiles, ... }: {
-        submodules = true;
     nixosConfigurations = {
       homelab = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
