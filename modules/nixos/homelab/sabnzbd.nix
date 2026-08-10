ed2053bfcd55323a2e4684d2d1ad76b1300629db
ArtLiathain <artp.oliathain@gmail.com>
2026-08-10 21:27:32 +0100

    feat(sabnzbd)

--- modules/nixos/homelab/sabnzbd.nix
+++ modules/nixos/homelab/sabnzbd.nix
@@ -4,6 +4,14 @@
   services.sabnzbd = {
     enable = true;
     openFirewall = true;
+    settings = {
+      misc = {
+        host = "0.0.0.0";
+        port = 8081;
+        # SABnzbd often blocks external access by default unless host checking is managed or disabled
+        host_whitelist = "*,";
+      };
+    };
   };
 
   systemd.services.sabnzbd.serviceConfig = {
