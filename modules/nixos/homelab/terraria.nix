f0da27b1b2a49743f966dc0f453d1a9170ec8d7f
ArtLiathain <artp.oliathain@gmail.com>
2026-09-08 22:23:10 +0100

    change server version

--- modules/nixos/homelab/terraria.nix
+++ modules/nixos/homelab/terraria.nix
@@ -5,7 +5,7 @@ let
   steamAppId = "1281930"; # tModLoader
   modpack = "default";
   packModsDir = "${dataDir}/ModPacks/${modpack}/Mods";
-  tmlVersion = "2026.06.03.6";
+  tmlVersion = "2026.06.3.6";
 
   workshopIds = [
     "2669644269"
